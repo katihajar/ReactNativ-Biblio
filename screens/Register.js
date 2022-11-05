50ca22c3c266c1382c7d68f643f00dcdc233449e
kati hajar <hakati1999@hotmail.com>
2022-11-05 20:54:36 +0100

    cc

--- screens/Register.js
+++ screens/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import axios from 'axios';
 import {
@@ -20,206 +20,227 @@ const DismissKeyboard = ({ children }) => (
   <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>{children}</TouchableWithoutFeedback>
 );
 
-const Register = (props) =>{
-  
+const Register = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
+  const [email, setEmail] = useState('');
   const { navigation } = props;
-    return (
-      <DismissKeyboard>
-        <Block flex middle>
-          <ImageBackground
-            source={Images.RegisterBackground}
-            style={styles.imageBackgroundContainer}
-            imageStyle={styles.imageBackground}
-          >
-            <Block flex middle>
-              <Block style={styles.registerContainer}>
-                <Block flex space="evenly">
-                  <Block flex={0.4} middle style={styles.socialConnect}>
-                    <Block flex={0.5} middle>
-                      <Text
-                        style={{
-                          fontFamily: 'montserrat-regular',
-                          textAlign: 'center'
-                        }}
-                        color="#333"
-                        size={24}
-                      >
-                        Register
-                      </Text>
-                    </Block>
-
-                    <Block flex={0.5} row middle space="between" style={{ marginBottom: 18 }}>
-                      <GaButton
-                        round
-                        onlyIcon
-                        shadowless
-                        icon="google"
-                        iconFamily="Font-Awesome"
-                        iconColor={theme.COLORS.WHITE}
-                        iconSize={theme.SIZES.BASE * 1.625}
-                        color={nowTheme.COLORS.GOOGLE}
-                        style={[styles.social, styles.shadow]}
-                      />
-
-                      <GaButton
-                        round
-                        onlyIcon
-                        shadowless
-                        icon="linkedin"
-                        iconFamily="Font-Awesome"
-                        iconColor={theme.COLORS.WHITE}
-                        iconSize={theme.SIZES.BASE * 1.625}
-                        color={nowTheme.COLORS.LINKEDIN}
-                        style={[styles.social, styles.shadow]}
-                      />
-                      <GaButton
-                        round
-                        onlyIcon
-                        shadowless
-                        icon="facebook"
-                        iconFamily="Font-Awesome"
-                        iconColor={theme.COLORS.WHITE}
-                        iconSize={theme.SIZES.BASE * 1.625}
-                        color={nowTheme.COLORS.FACEBOOK}
-                        style={[styles.social, styles.shadow]}
-                      />
-                    </Block>
-                  </Block>
-                  <Block  middle>
+  return (
+    <DismissKeyboard>
+      <Block flex middle>
+        <ImageBackground
+          source={Images.RegisterBackground}
+          style={styles.imageBackgroundContainer}
+          imageStyle={styles.imageBackground}
+        >
+          <Block flex middle>
+            <Block style={styles.registerContainer}>
+              <Block flex space="evenly">
+                <Block flex={0.4} middle style={styles.socialConnect}>
+                  <Block flex={0.5} middle>
                     <Text
                       style={{
                         fontFamily: 'montserrat-regular',
                         textAlign: 'center'
                       }}
-                      muted
-                      size={16}
+                      color="#333"
+                      size={24}
                     >
-                      or be classical
+                      Register
                     </Text>
                   </Block>
-                  
-                  <Block flex={1} middle space="between">
-                    <Block center flex={0.9}>
-                      <Block flex space="between">
-                        <Block>
-                          
-                          <Block width={width * 0.8} >
-                            <Input
-                              placeholder="First Name"
-                              style={styles.inputs}
-                              iconContent={
-                                <Icon
-                                  size={16}
-                                  color="#ADB5BD"
-                                  name="profile-circle"
-                                  family="NowExtra"
-                                  style={styles.inputIcons}
-                                />
+
+                  <Block flex={0.5} row middle space="between" style={{ marginBottom: 18 }}>
+                    <GaButton
+                      round
+                      onlyIcon
+                      shadowless
+                      icon="google"
+                      iconFamily="Font-Awesome"
+                      iconColor={theme.COLORS.WHITE}
+                      iconSize={theme.SIZES.BASE * 1.625}
+                      color={nowTheme.COLORS.GOOGLE}
+                      style={[styles.social, styles.shadow]}
+                    />
+
+                    <GaButton
+                      round
+                      onlyIcon
+                      shadowless
+                      icon="linkedin"
+                      iconFamily="Font-Awesome"
+                      iconColor={theme.COLORS.WHITE}
+                      iconSize={theme.SIZES.BASE * 1.625}
+                      color={nowTheme.COLORS.LINKEDIN}
+                      style={[styles.social, styles.shadow]}
+                    />
+                    <GaButton
+                      round
+                      onlyIcon
+                      shadowless
+                      icon="facebook"
+                      iconFamily="Font-Awesome"
+                      iconColor={theme.COLORS.WHITE}
+                      iconSize={theme.SIZES.BASE * 1.625}
+                      color={nowTheme.COLORS.FACEBOOK}
+                      style={[styles.social, styles.shadow]}
+                    />
+                  </Block>
+                </Block>
+                <Block middle>
+                  <Text
+                    style={{
+                      fontFamily: 'montserrat-regular',
+                      textAlign: 'center'
+                    }}
+                    muted
+                    size={16}
+                  >
+                    or be classical
+                  </Text>
+                </Block>
+
+                <Block flex={1} middle space="between">
+                  <Block center flex={0.9}>
+                    <Block flex space="between">
+                      <Block>
+
+                        <Block width={width * 0.8} >
+                          <Input
+                            placeholder="username"
+                            style={styles.inputs}
+                            onChangeText={newText => setUsername(newText)}
+                            iconContent={
+                              <Icon
+                                size={16}
+                                color="#ADB5BD"
+                                name="profile-circle"
+                                family="NowExtra"
+                                style={styles.inputIcons}
+                              />
+                            }
+                          />
+                        </Block>
+                        <Block width={width * 0.8}>
+                          <Input
+                            placeholder="Email"
+                            style={styles.inputs}
+                            onChangeText={newText => setEmail(newText)}
+                            iconContent={
+                              <Icon
+                                size={16}
+                                color="#ADB5BD"
+                                name="email-852x"
+                                family="NowExtra"
+                                style={styles.inputIcons}
+                              />
+                            }
+                          />
+                        </Block>
+                        <Block width={width * 0.8}>
+                          <Input
+                            placeholder="Phone Number"
+                            onChangeText={newText => setPhone(newText)}
+                            style={styles.inputs}
+                            iconContent={
+                              <Icon
+                                size={16}
+                                color="#ADB5BD"
+                                name="mobile2x"
+                                family="NowExtra"
+                                style={styles.inputIcons}
+                              />
+                            }
+                          />
+                        </Block>
+                        <Block width={width * 0.8}>
+                          <Input
+                            placeholder="Password"
+                            style={styles.inputs}
+                            onChangeText={newText => setPassword(newText)}
+                            iconContent={
+                              <Icon
+                                size={16}
+                                color="#ADB5BD"
+                                name="lock-circle-open2x"
+                                family="NowExtra"
+                                style={styles.inputIcons}
+                              />
+                            }
+                          />
+                        </Block>
+                        <Block
+                          style={{ marginVertical: theme.SIZES.BASE, marginLeft: 15 }}
+                          row
+                          width={width * 0.75}
+                        >
+                          <Checkbox
+                            checkboxStyle={{
+                              borderWidth: 1,
+                              borderRadius: 2,
+                              borderColor: '#E3E3E3'
+                            }}
+                            color={nowTheme.COLORS.PRIMARY}
+                            labelStyle={{
+                              color: nowTheme.COLORS.HEADER,
+                              fontFamily: 'montserrat-regular'
+                            }}
+                            label="I agree to the terms and conditions."
+                          />
+                        </Block>
+                      </Block>
+                      <Block center>
+                        <Button color="primary" round style={styles.createButton} onPress={() => {
+                          console.log("login");
+                          fetch('http://10.214.43.93:8036/api/auth/signup', {
+                            method: 'POST',
+                            headers: {
+                              Accept: 'application/json',
+                              'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({
+                              username: username,
+                              password: password,
+                              phone: phone,
+                              email: email,
+                              universite: {
+                                id: "636131b519c13795f9146377",
+                                libelle: "university",
+                                addresse: "addresse"
                               }
-                            />
-                          </Block>
-                          <Block width={width * 0.8} style={{ marginBottom: 5 }}>
-                            <Input
-                              placeholder="Last Name"
-                              style={styles.inputs}
-                              iconContent={
-                                <Icon
-                                  size={16}
-                                  color="#ADB5BD"
-                                  name="caps-small2x"
-                                  family="NowExtra"
-                                  style={styles.inputIcons}
-                                />
+                            })
+                          }).then((response) => response.json())
+                            .then((data) => {
+                              console.log(data);
+                              if (data.accessToken != null) {
+                                navigation.navigate('SignIn');
                               }
-                            />
-                          </Block>
-                          <Block width={width * 0.8}>
-                            <Input
-                              placeholder="Email"
-                              style={styles.inputs}
-                              iconContent={
-                                <Icon
-                                  size={16}
-                                  color="#ADB5BD"
-                                  name="email-852x"
-                                  family="NowExtra"
-                                  style={styles.inputIcons}
-                                />
-                              }
-                            />
-                          </Block>
-                          <Block width={width * 0.8}>
-                            <Input
-                              placeholder="Phone Number"
-                              style={styles.inputs}
-                              iconContent={
-                                <Icon
-                                  size={16}
-                                  color="#ADB5BD"
-                                  name="mobile2x"
-                                  family="NowExtra"
-                                  style={styles.inputIcons}
-                                />
-                              }
-                            />
-                          </Block>
-                          <Block width={width * 0.8}>
-                            <Input
-                              placeholder="Password"
-                              style={styles.inputs}
-                              iconContent={
-                                <Icon
-                                  size={16}
-                                  color="#ADB5BD"
-                                  name="lock-circle-open2x"
-                                  family="NowExtra"
-                                  style={styles.inputIcons}
-                                />
-                              }
-                            />
-                          </Block>
-                          <Block
-                            style={{ marginVertical: theme.SIZES.BASE, marginLeft: 15}}
-                            row
-                            width={width * 0.75}
+                            })
+                            .catch((err) => {
+                              console.log(err);
+                            });
+                        }} >
+                          <Text
+                            style={{ fontFamily: 'montserrat-bold' }}
+                            size={14}
+                            color={nowTheme.COLORS.WHITE}
                           >
-                            <Checkbox
-                              checkboxStyle={{
-                                borderWidth: 1,
-                                borderRadius: 2,
-                                borderColor: '#E3E3E3'
-                              }}
-                              color={nowTheme.COLORS.PRIMARY}
-                              labelStyle={{
-                                color: nowTheme.COLORS.HEADER,
-                                fontFamily: 'montserrat-regular'
-                              }}
-                              label="I agree to the terms and conditions."
-                            />
-                          </Block>
-                        </Block>
-                        <Block center>
-                          <Button color="primary" round style={styles.createButton}   onPress={() => navigation.navigate('SignIn')}>
-                            <Text
-                              style={{ fontFamily: 'montserrat-bold' }}
-                              size={14}
-                              color={nowTheme.COLORS.WHITE}
-                            >
-                              Get Started
-                            </Text>
-                          </Button>
-                        </Block>
+                            Get Started
+                          </Text>
+                        </Button>
                       </Block>
                     </Block>
                   </Block>
                 </Block>
               </Block>
             </Block>
-          </ImageBackground>
-        </Block>
-      </DismissKeyboard>
-    );
-  }
+          </Block>
+        </ImageBackground>
+      </Block>
+    </DismissKeyboard>
+  );
+}
 
 
 const styles = StyleSheet.create({
